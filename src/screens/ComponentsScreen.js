2153da7af5b0d1a5354da673af9ce2df6b81e5f0
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-08-19 21:54:22 -0700

    added Image component

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
-
-const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the ComponentsScreen</Text>
-}
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { AuthSession } from 'expo';
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    alignItems: "center"
+  },
+  logo: {
+    marginTop: 20,
+    width: 300,
+    height: 300,
+  },
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
+    color: 'rgb(234,111,222)'
   }
-})
+});
+
+const ComponentsScreen = () => (
+    < View style = { styles.container } >
+      <Text style={styles.textStyle}>学习使我快乐</Text>
+
+    <Image
+      style={styles.logo}
+      source={{
+        uri: 'https://www.keaidian.com/uploads/allimg/190520/20224455_0.jpg',
+      }}
+    />
+    </View >
+)
+
+
 
 export default ComponentsScreen;
