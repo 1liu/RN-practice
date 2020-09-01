3c88d557916fbae566e9ba37d97b25083f353a03
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-09-01 15:05:26 -0700

    using ternary operator to setColor

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,31 +2,48 @@ import React, { useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const INCREMENT = 10;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(Math.floor(Math.random() * 256));
   const [green, setGreen] = useState(Math.floor(Math.random() * 256));
   const [blue, setBlue] = useState(Math.floor(Math.random() * 256));
+
+  const setColor = (color, change) => {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
+    }
+  }
+
   return (
     <View>
       <ColorCounter
         color="Red"
         number={red}
-        onIncrease={() => { if (red < 255) setRed(red + 1) }}
-        onDecrease={() => { if (red > 0) setRed(red - 1) }}
+        onIncrease={() => { setColor('red', INCREMENT) }}
+        onDecrease={() => { setColor('red', -1 * INCREMENT) }}
       />
       <ColorCounter
         color="Green"
         number={green}
-        onIncrease={() => { if (green < 255) setGreen(green + 1) }}
-        onDecrease={() => { if (green > 0) setGreen(green - 1) }}
+        onIncrease={() => { setColor('green', INCREMENT) }}
+        onDecrease={() => { setColor('green', -1 * INCREMENT) }}
       />
       <ColorCounter
         color="Blue"
         number={blue}
-        onIncrease={() => { if (blue < 255) setBlue(blue + 1) }}
-        onDecrease={() => { if (blue > 0) setBlue(blue - 1) }}
+        onIncrease={() => { setColor('blue', INCREMENT) }}
+        onDecrease={() => { setColor('blue', -1 * INCREMENT) }}
       />
-      <View style={{height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+      <View style={{ height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
     </View>
   )
 }
